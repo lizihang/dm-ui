6859318338367407ac811fce876710e7d79d92d6
lizihang <lizihang@snsoft.com.cn>
2020-09-24 16:45:21 +0800

    将container拆分

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,76 +2,14 @@
   <div class="home">
     <el-container>
       <el-aside width="200px">
-        <el-menu default-active="1" class="el-menu-vertical-demo" :unique-opened=true @open="handleOpen"
-                 @close="handleClose"
-                 background-color="#409EFF" text-color="#ffffff" active-text-color="#ffd04b">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航二</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3">
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
-        </el-menu>
+        <dm-menu></dm-menu>
       </el-aside>
       <el-container>
         <el-header style="text-align: right; font-size: 16px">
-          <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-          <span>王小虎</span>
+          <dm-header></dm-header>
         </el-header>
         <el-main>
-          <el-table :data="tableData" border style="width: 100%">
-            <el-table-column prop="date" label="日期" width="180">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址">
-            </el-table-column>
-          </el-table>
+          <dm-main></dm-main>
         </el-main>
       </el-container>
     </el-container>
@@ -85,31 +23,22 @@
   }
 
   .el-header {
-    background-color: #B3C0D1;
+    background-color: #409EFF;
     color: #333;
     line-height: 60px;
   }
 </style>
 
 <script>
+  import DmMenu from "./frame/DmMenu";
+  import DmHeader from "./frame/DmHeader";
+  import DmMain from "./frame/DmMain";
+
   export default {
-    data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
-      return {
-        tableData: Array(5).fill(item)
-      }
-    },
-    methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      }
+    components: {
+      DmMenu,
+      DmHeader,
+      DmMain
     }
   };
 </script>
