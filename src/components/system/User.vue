26c408d290249e77d70b59d7fdfea23091c76b23
lizihang <lizihang@snsoft.com.cn>
2020-10-13 00:02:29 +0800

    增加axions

--- src/components/system/User.vue
+++ src/components/system/User.vue
@@ -3,12 +3,11 @@
     <el-button type="primary" @click="show = !show">新增用户</el-button>
     <el-button type="primary" @click="dialogFormVisible = true">新增用户弹窗</el-button>
     <el-table :data="tableData" border stripe style="width: 100%; margin-top: 20px">
-      <el-table-column prop="date" label="日期" width="180">
-      </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180">
-      </el-table-column>
-      <el-table-column prop="address" label="地址">
-      </el-table-column>
+      <el-table-column prop="id" label="id" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="birth" label="生日" width="180"></el-table-column>
+      <el-table-column prop="gender" label="性别" width="180"></el-table-column>
+      <el-table-column prop="addr" label="地址"></el-table-column>
     </el-table>
     <div style="margin-top: 20px; height: 200px;">
       <el-collapse-transition>
@@ -47,8 +46,7 @@
           <el-input v-model="form.name"></el-input>
         </el-form-item>
         <el-form-item label="生日">
-          <el-date-picker type="date" placeholder="选择日期" v-model="form.birth"
-                          style="width: 100%;"></el-date-picker>
+          <el-date-picker type="date" placeholder="选择日期" v-model="form.birth" style="width: 100%;"></el-date-picker>
         </el-form-item>
         <el-form-item label="性别">
           <el-radio-group v-model="form.gender">
@@ -70,16 +68,13 @@
 </template>
 
 <script>
+  import axios from "axios";
+
   export default {
     name: "User",
     data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
       return {
-        tableData: Array(5).fill(item),
+        tableData: [],
         show: false,
         dialogFormVisible: false,
         form: {
@@ -92,8 +87,39 @@
     },
     methods: {
       onSubmit() {
-        this.dialogFormVisible = false;
+        //1 发送请求
+        axios({
+          url: 'http://127.0.0.1:8081/user/save',
+          method: 'post',
+          data: this.form
+        }).then(res => {
+          console.log(res.data);
+          if (res.data.status) {
+            this.$message({
+              message: res.data.msg,
+              type: 'success'
+            });
+            // 清空表单信息
+            this.form = {gender: '男'};
+            // 隐藏表单
+            this.dialogFormVisible = false;
+            // 刷新数据
+            this.findAll()
+          } else {
+            this.$message.error(res.data.msg);
+          }
+        })
+      },
+      findAll() {
+        axios({
+          url: 'http://127.0.0.1:8081/user/findAll'
+        }).then(res => {
+          this.tableData = res.data
+        })
       }
+    },
+    created() {
+      this.findAll()
     }
   }
 </script>
