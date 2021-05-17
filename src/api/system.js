868885bcbc0874b1fb7d481ff483e30b6aedabe9
lizihang <lzhsmart@163.com>
2021-05-18 00:05:10 +0800

    字典的新增和修改

--- src/api/system.js
+++ src/api/system.js
@@ -11,12 +11,28 @@ export function queryDictList(param) {
 }
 
 // 根据字典ID获取字典详情
-export function queryDict(dictId) {
+export function queryDictInfo(dictId) {
   return axios({
     url: 'http://127.0.0.1:8081/system/queryDictInfo',
     method: 'get',
     params: {
       dictId: dictId
     }
+  })
+}
+
+export function addDict(dict) {
+  return axios({
+    url: 'http://127.0.0.1:8081/system/addDict',
+    method: 'post',
+    data: dict
+  })
+}
+
+export function updateDict(dict) {
+  return axios({
+    url: 'http://127.0.0.1:8081/system/updateDict',
+    method: 'post',
+    data: dict
   })
 }
--- src/components/system/DictConfig.vue
+++ src/components/system/DictConfig.vue
@@ -10,10 +10,10 @@
       <el-form-item label="状态">
         <el-select v-model="param.status" placeholder="">
           <el-option
-            v-for="state in dict_status"
-            :key="state.dictKey"
-            :label="state.dictValue"
-            :value="state.dictKey">
+                  v-for="state in dict_status"
+                  :key="state.dictKey"
+                  :label="state.dictValue"
+                  :value="state.dictKey">
           </el-option>
         </el-select>
       </el-form-item>
@@ -44,69 +44,48 @@
       <el-table-column prop="modifyTime" label="修改时间" width="180"></el-table-column>
     </el-table>
     <el-pagination
-      style="float: right"
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      background
-      :total="total"
-      :current-page="param.pageNum"
-      :page-size="param.pageSize"
-      :page-sizes="[1, 5, 10, 20, 50, 100]"
-      layout="total, sizes, prev, pager, next, jumper">
+            style="float: right"
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            background
+            :total="total"
+            :current-page="param.pageNum"
+            :page-size="param.pageSize"
+            :page-sizes="[1, 5, 10, 20, 50, 100]"
+            layout="total, sizes, prev, pager, next, jumper">
     </el-pagination>
-    <!-- 新增form -->
-    <el-dialog title="新增" :visible.sync="addFormVisible">
-      <el-form ref="form" :model="addForm" label-width="80px">
-        <el-form-item label="昵称">
-          <el-input v-model="updateForm.nickname"></el-input>
+
+    <!-- 新增，修改dialog -->
+    <el-dialog :title="dialogTitle" :visible.sync="dialogVisible">
+      <el-form ref="form" :model="dialogForm" :rules="rules" label-width="80px">
+        <el-form-item label="字典号" prop="dictId">
+          <el-input v-model="dialogForm.dictId"></el-input>
         </el-form-item>
-        <el-form-item label="邮箱">
-          <el-input v-model="updateForm.email"></el-input>
-        </el-form-item>
-        <el-form-item label="状态">
-          <el-select v-model="updateForm.status" placeholder="">
-            <el-option
-              v-for="state in dict_status"
-              :key="state.dictKey"
-              :label="state.dictValue"
-              :value="state.dictKey">
-            </el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="">确 定</el-button>
-        <el-button @click="updateFormVisible = false">取 消</el-button>
-      </div>
-    </el-dialog>
-    <!--更新form-->
-    <el-dialog title="修改" :visible.sync="updateFormVisible">
-      <el-form ref="form" :model="updateForm" label-width="80px">
-        <el-form-item label="字典名称">
-          <el-input v-model="updateForm.dictName"></el-input>
+        <el-form-item label="字典名称" prop="dictName">
+          <el-input v-model="dialogForm.dictName"></el-input>
         </el-form-item>
         <el-form-item label="备注">
-          <el-input v-model="updateForm.remark"></el-input>
+          <el-input v-model="dialogForm.remark"></el-input>
         </el-form-item>
         <el-form-item label="状态">
-          <el-radio v-model="updateForm.status" label="10">未启用</el-radio>
-          <el-radio v-model="updateForm.status" label="70">启用</el-radio>
+          <el-radio v-model="dialogForm.status" label="10">未启用</el-radio>
+          <el-radio v-model="dialogForm.status" label="70">启用</el-radio>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="">确 定</el-button>
-        <el-button @click="updateCancel">取 消</el-button>
+        <el-button type="primary" @click="submit">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
-  
+
     <!--详情form-->
-    <el-dialog title="详细信息" :visible.sync="detailFormVisible">
-      <el-form ref="form" :model="detailList[0]" label-width="80px">
-        <el-form-item label="字典名称">
-          <el-input v-model="detailList[0].dictKey"></el-input>
+    <el-dialog title="详细信息" :visible.sync="detailFormVisible" width="500px">
+      <el-form :inline="true" ref="detailForm" :model="detail" v-for="detail in detailList" label-width="80px" class="detail-form-inline">
+        <el-form-item label="字典值" size="mini">
+          <el-input v-model="detail.dictKey" class="detail-input" size="mini"></el-input>
         </el-form-item>
-        <el-form-item label="字典值">
-          <el-input v-model="detailList[0].dictValue"></el-input>
+        <el-form-item label="字典名" size="mini">
+          <el-input v-model="detail.dictValue" class="detail-input" size="mini"></el-input>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -114,139 +93,205 @@
 </template>
 
 <script>
-import {queryDictList, queryDict} from "@/api/system";
-import {selectDictValue} from "@/utils";
+  import {queryDictList, queryDictInfo, addDict, updateDict} from "@/api/system";
+  import {selectDictValue} from "@/utils";
 
-export default {
-  name: "DictConfig",
-  data() {
-    return {
-      radioId: '',
-      // 分页数据
-      total: 0,
-      // 查询条件
-      param: {
-        dictID: '',
-        dictName: '',
-        pageNum: 1,
-        pageSize: 10,
-      },
-      // 列表数据
-      tableData: [],
-      // 当前选中行的数据
-      currentRow: {},
-      oldCurrentRow: {},
-      // 状态字典
-      dict_status: [],
-      // 更新dialog相关
-      addFormVisible: false,
-      addForm: {},
-      // 更新dialog相关
-      updateFormVisible: false,
-      updateForm: {
-        remark: '',
-      },
-      isEnable: false,
-      detailFormVisible: false,
-      detailList: [{
-        dictKey: '',
-        dictValue: '',
-      }],
-    }
-  },
-  methods: {
-    // 查询数据
-    findAll() {
-      queryDictList(this.param).then(res => {
-        this.tableData = res.data.data.list
-        this.total = res.data.data.total
-        // 处理单选radio
-        this.currentRow = res.data.data.list[0]
-        this.radioId = this.currentRow.dictId
-      })
-    },
-    // 查询状态字典
-    getStatus() {
-      queryDict("dict_status").then(res => {
-        console.log(res.data.data)
-        this.dict_status = res.data.data
-      })
-    },
-    // 查询按钮
-    onSubmit() {
-      this.findAll();
-    },
-    // 重置按钮
-    onReset() {
-      this.param = {
-        pageNum: 1,
-        pageSize: 10
-      };
-      this.findAll();
-    },
-    // 处理分页，当前页大小改变
-    handleSizeChange(val) {
-      this.param.pageSize = val;
-      this.findAll();
-    },
-    // 处理分页，当前页数改变
-    handleCurrentChange(val) {
-      this.param.pageNum = val;
-      this.findAll();
-    },
-    // 字典状态字典翻译
-    statusFormatter(row, column) {
-      return selectDictValue(this.dict_status, row.status);
-    },
-    // 处理table行改变
-    handleRowChange(val) {
-      // 切换分页的时候，val为空，所以判断一下
-      if (val == null) {
-        this.currentRow = this.tableData[0];
-        this.radioId = this.currentRow.dictId;
-      } else {
-        this.currentRow = val;
-        this.radioId = val.dictId;
+  export default {
+    name: "DictConfig",
+    data() {
+      return {
+        radioId: '',
+        // 分页数据
+        total: 0,
+        // 查询条件
+        param: {
+          dictID: '',
+          dictName: '',
+          pageNum: 1,
+          pageSize: 10,
+        },
+        // 列表数据
+        tableData: [],
+        // 当前选中行的数据
+        currentRow: {},
+        // 状态字典
+        dict_status: [],
+        // dialog相关
+        dialogTitle: '',
+        dialogVisible: false,
+        // flag = true 表单为新增，flag = false
+        dialogFlag: '',
+        dialogForm: {
+          remark: '',
+          status: '70'
+        },
+        // 表单校验
+        rules: {
+          dictId: [
+            {required: true, message: "字典号不能为空", trigger: "blur"}
+          ],
+          dictName: [
+            {required: true, message: "字典名称不能为空", trigger: "blur"}
+          ]
+        },
+        // 详情相关
+        detailFormVisible: false,
+        detailList: [{
+          dictKey: '',
+          dictValue: '',
+        }],
       }
     },
-    openAddDialog() {
-      this.addFormVisible = true;
+    methods: {
+      // 查询数据
+      findAll() {
+        queryDictList(this.param).then(res => {
+          this.tableData = res.data.data.list
+          this.total = res.data.data.total
+          // 处理单选radio
+          this.currentRow = res.data.data.list[0]
+          this.radioId = this.currentRow.dictId
+        })
+      },
+      // 查询状态字典
+      getStatus() {
+        queryDictInfo("dict_status").then(res => {
+          this.dict_status = res.data.data
+        })
+      },
+      // 查询按钮
+      onSubmit() {
+        this.findAll();
+      },
+      // 重置按钮
+      onReset() {
+        this.param = {
+          pageNum: 1,
+          pageSize: 10
+        };
+        this.findAll();
+      },
+      // 处理分页，当前页大小改变
+      handleSizeChange(val) {
+        this.param.pageSize = val;
+        this.findAll();
+      },
+      // 处理分页，当前页数改变
+      handleCurrentChange(val) {
+        this.param.pageNum = val;
+        this.findAll();
+      },
+      // 字典状态字典翻译
+      statusFormatter(row, column) {
+        return selectDictValue(this.dict_status, row.status);
+      },
+      // 处理table行改变
+      handleRowChange(val) {
+        // 切换分页的时候，val为空，所以判断一下
+        if (val == null) {
+          this.currentRow = this.tableData[0];
+          this.radioId = this.currentRow.dictId;
+        } else {
+          this.currentRow = val;
+          this.radioId = val.dictId;
+        }
+      },
+      // 打开新建界面
+      openAddDialog() {
+        this.dialogTitle = '新增';
+        this.dialogVisible = true;
+        this.dialogFlag = true;
+        this.dialogForm = {
+          status: '70'
+        }
+      },
+      // 打开修改界面
+      openUpdateDialog() {
+        this.dialogTitle = '修改';
+        this.dialogVisible = true;
+        this.dialogFlag = false;
+        // Object.assign()解决双向绑定问题：在修改dialog修改数据后点击取消，主界面table数据改变的问题
+        this.dialogForm = Object.assign({}, this.currentRow);
+      },
+      // 表单提交，新增或修改
+      submit() {
+        this.$refs['form'].validate(valid => {
+          if (valid) {
+            // true 新增
+            if (this.dialogFlag) {
+              addDict(this.dialogForm).then(res => {
+                if (res.data.status === 200) {
+                  this.$message({
+                    message: res.data.msg,
+                    type: 'success'
+                  });
+                  // 隐藏表单
+                  this.dialogVisible = false;
+                  // 刷新数据
+                  this.findAll()
+                } else {
+                  this.$message.error(res.data.msg);
+                }
+              })
+            } else {
+              updateDict(this.dialogForm).then(res => {
+                if (res.data.status === 200) {
+                  this.$message({
+                    message: res.data.msg,
+                    type: 'success'
+                  });
+                  // 隐藏表单
+                  this.dialogVisible = false;
+                  // 刷新数据
+                  this.findAll()
+                } else {
+                  this.$message.error(res.data.msg);
+                }
+              })
+            }
+            //  清空form
+            this.dialogForm = {
+              status: '70'
+            };
+          }
+        });
+      },
+      // 表单取消按钮
+      cancel() {
+        this.$refs['form'].clearValidate();
+        this.dialogVisible = false;
+      },
+      // 删除字典
+      deleteDict() {
+
+      },
+      openDetail() {
+        this.detailFormVisible = true;
+        console.log(this.currentRow);
+        this.detailList = this.currentRow.dictInfoList;
+      }
     },
-    // 新增字典
-    addDict() {
-    
-    },
-    // 打开修改界面
-    openUpdateDialog() {
-      this.updateFormVisible = true;
-      this.updateForm = this.currentRow;
-      this.oldCurrentRow = this.currentRow;
-    },
-    // 修改字典
-    editDict() {
-      this.updateFormVisible = true;
-    },
-    updateCancel(){
-      this.currentRow = this.oldCurrentRow;
-      this.updateFormVisible = false;
-    },
-    // 删除字典
-    deleteDict() {
-    
-    },
-    openDetail(){
-      this.detailFormVisible =true;
-      console.log(this.currentRow);
-      this.detailList = this.currentRow.dictInfoList;
+    created() {
+      this.findAll();
+      this.getStatus();
     }
-  },
-  created() {
-    this.findAll();
-    this.getStatus();
   }
-}
 </script>
 
-<style scoped>
+<style>
+  .detail-input {
+    width: 120px;
+  }
 
+  .detail-toolbar {
+    padding-bottom: 10px;
+  }
+
+  .el-dialog__body {
+    padding-top: 0px;
+  }
+
+  .detail-form-inline {
+    height: 40px;
+  }
 </style>
