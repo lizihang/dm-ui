ee90d5aa6308bd07a42f348199f36a8151d76342
lizihang <lzhsmart@163.com>
2021-05-27 14:54:38 +0800

    测试严格模式

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,10 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 在严格模式下，无论何时发生了状态变更且不是由 mutation 函数引起的，将会抛出错误。
+  // strict: true,
+  // 生产环境应该关闭，有性能损失
+  // strict: process.env.NODE_ENV !== 'production',
   state: {
     user: {
       username: '',
@@ -38,6 +42,8 @@ export default new Vuex.Store({
       }
     ],
     activeIndex: '1',
+    // 测试严格模式
+    testStrict: 1,
   },
   mutations: {
     // 添加菜单
